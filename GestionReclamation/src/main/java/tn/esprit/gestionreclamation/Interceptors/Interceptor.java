80e5ea693d6a9aa80afb36f0410c7a6a3234b87c
yassine <sassou.est100@gmail.com>
2023-04-05 19:34:16 +0100

    fixed interceptor

--- GestionReclamation/src/main/java/tn/esprit/gestionreclamation/Interceptors/Interceptor.java
+++ GestionReclamation/src/main/java/tn/esprit/gestionreclamation/Interceptors/Interceptor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import tn.esprit.gestionreclamation.models.Users;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class Interceptor implements HandlerInterceptor {
     private final Authentication authentication;
     private final UserService userService;
@@ -24,12 +26,16 @@ public class Interceptor implements HandlerInterceptor {
             HttpServletResponse httpServletResponse,
             Object handler
     ){
+        log.info("Running Auth Interceptor");
+        log.info("Request email: {}",httpServletRequest.getHeader("email"));
         Optional<Users> user = userService.getUserByUserName(httpServletRequest.getHeader("email"));
         String authorities = httpServletRequest.getHeader("authorities");
         if(user.isEmpty()) throw new EntityNotFoundException("User Not Found");
         authentication.setName(user.get().getEmail());
         authentication.setProfile(user.get());
         authentication.setAuthorities(authorities);
+        log.info("Auth username: {}", authentication.getName());
         return true;
     }
+
 }
