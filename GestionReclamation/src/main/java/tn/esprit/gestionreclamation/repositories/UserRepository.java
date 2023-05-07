7c86e0579317bef59feb41470c9d8874f3a513bd
yassine <sassou.est100@gmail.com>
2023-05-07 12:44:53 +0100

    fixed find by Id

--- GestionReclamation/src/main/java/tn/esprit/gestionreclamation/repositories/UserRepository.java
+++ GestionReclamation/src/main/java/tn/esprit/gestionreclamation/repositories/UserRepository.java
@@ -12,4 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
     List<Users> findAllByRole(Role role);
+
+    Optional<Users> findById(Long Id);
 }
