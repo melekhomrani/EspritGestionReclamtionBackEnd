62e76afcf01527ee4ea6c91cc85b8b518d9a0c45
melekhomrani <melekhomrani87@gmail.com>
2023-05-10 12:42:33 +0100

    Revert "update delete user"
    This reverts commit 373362134bb27ab07df52b7fb58957edba13d53b.

--- GestionReclamation/src/main/java/tn/esprit/gestionreclamation/config/WebMvcConfig.java
+++ GestionReclamation/src/main/java/tn/esprit/gestionreclamation/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package tn.esprit.gestionreclamation.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tn.esprit.gestionreclamation.Interceptors.Interceptor;
@@ -15,14 +14,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor);
-    }
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
     }
 }
